7266f98ea11c200ba0b21dc39fff2afbfb08ca2e
lee_c <gitleelee@gmail.com>
2023-11-16 15:07:38 -0800

    Adding object properties with dot notation.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+// Object & Properties with Dot Notation
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = [
+    "Learning coding essentials",
+    "Using more efficient tools",
+    "Relaxing",
+    "Build confidence"
+]
+learning.catagory = "Front End Development"
+learning.topicImportance = "High";
